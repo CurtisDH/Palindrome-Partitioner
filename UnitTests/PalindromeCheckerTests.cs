e0db966ad9e98016bf38e631cfb2d97e45c48916
CurtisDH <curtisdh0@gmail.com>
2023-07-23 20:46:48 +1000

    Added PalindromeChecker unit testing

--- UnitTests/PalindromeCheckerTests.cs
+++ UnitTests/PalindromeCheckerTests.cs
@@ -1,19 +1,48 @@
 using System;
+using Palindrome_Partitioner;
+using Xunit;
 using Xunit.Abstractions;
 
-namespace UnitTests;
-
-public class PalindromeCheckerTests : IDisposable
+namespace UnitTests
 {
-    private ITestOutputHelper _output;
-
-
-    public PalindromeCheckerTests(ITestOutputHelper output)
+    public class PalindromeCheckerTests : IDisposable
     {
-        _output = output;
-    }
+        private ITestOutputHelper _output;
+        private PalindromeChecker _checker;
 
-    public void Dispose()
-    {
+        public PalindromeCheckerTests(ITestOutputHelper output)
+        {
+            _output = output;
+            Setup();
+        }
+
+        void Setup()
+        {
+            _checker = new PalindromeChecker();
+        }
+
+        [Theory]
+        [InlineData("", true)] // Empty string
+        [InlineData("a", true)] // Single character
+        [InlineData("aa", true)] // Two same characters
+        [InlineData("ab", false)] // Two different characters
+        [InlineData("racecar", true)] // Multi-character palindrome
+        [InlineData("hello", false)] // Multi-character non-palindrome
+        public void IsPalindromeTest(string inp, bool expected)
+        {
+            var result = _checker.IsPalindrome(inp, 0, inp.Length - 1);
+
+            _output.WriteLine($"Tested string: '{inp}'");
+            _output.WriteLine($"Expected result: {expected}");
+            _output.WriteLine($"Actual result: {result}");
+
+            Assert.Equal(expected, result);
+        }
+
+        public void Dispose()
+        {
+            _checker = null;
+            _output = null;
+        }
     }
 }
